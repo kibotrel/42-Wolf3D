dc681b6fdc24eeb72f2fbcb8d3bc5c10f787b30d
Lola ROTA <grota@e1r8p13.42.fr>
2019-04-02 15:30:58 +0200

    include file created

---
+++ incs/env.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: grota <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/02 15:23:35 by grota             #+#    #+#             */
+/*   Updated: 2019/04/02 15:30:21 by grota            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ENV_H
+# define ENV_H
+
+/*
+**		Window parameters
+*/
+
+# define WIN_WIDTH	1920
+# define WIN_HEIGHT	1080
+
+/*
+**		Basic colors
+*/
+
+# define BLACK		0x000000
+# define WHITE		0xFFFFFF
+# define RED		0xFF0000
+# define GREEN		0x00FF00
+# define BLUE		0x0000FF
+# define MAGENTA	0xFF00FF
+# define YELLOW		0xFFFF00
+# define CYAN		0x00FFFF
+
+#endif
