fdde960807433fd4b957c05591abd55319022586
Kilian BOTREL <kibotrel@e1r8p12.42.fr>
2019-04-02 16:25:12 +0200

    Added Error when the binary is ran from an incorrect directory

--- incs/env.h
+++ incs/env.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   env.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: grota <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: grota <grota@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/02 15:23:35 by grota             #+#    #+#             */
-/*   Updated: 2019/04/02 15:30:21 by grota            ###   ########.fr       */
+/*   Updated: 2019/04/02 16:19:13 by kibotrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 # define ENV_H
 
 /*
-**		Window parameters
+**	Window parameters
 */
 
 # define WIN_WIDTH	1920
 # define WIN_HEIGHT	1080
 
 /*
-**		Basic colors
+**	Basic colors
 */
 
 # define BLACK		0x000000
@@ -33,4 +33,9 @@
 # define YELLOW		0xFFFF00
 # define CYAN		0x00FFFF
 
+/*
+**	Error codes
+*/
+
+# define ERR_EXE	"\033[31;1mError:\033[0m Wrong running directory."
 #endif
--- srcs/core/main.c
+++ srcs/core/main.c
@@ -6,17 +6,22 @@
 /*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/02 14:57:54 by kibotrel          #+#    #+#             */
-/*   Updated: 2019/04/02 15:45:49 by kibotrel         ###   ########.fr       */
+/*   Updated: 2019/04/02 16:19:59 by kibotrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "env.h"
 #include "wolf3d.h"
 
 int	main(int ac, char **av)
 {
-	if (ac == 2 && !ft_strcmp(av[0], "./wolf3d"))
+	if (ac == 2)
+	{
+		if (ft_strcmp(av[0], "./wolf3d"))
+			ft_print_error(ERR_EXE, 1);
 		ft_putendl("Parsing incoming");
+	}
 	else
 		usage();
 	return (0);
