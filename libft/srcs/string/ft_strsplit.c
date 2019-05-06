016402e50562eab10e8d3a4de7bb70d39b977f4b
Kilian BOTREL <kibotrel@e2r10p22.42.fr>
2019-05-06 17:58:51 +0200

    Protected strsplit from leaking if malloc can't allocate memory

--- libft/srcs/string/ft_strsplit.c
+++ libft/srcs/string/ft_strsplit.c
@@ -6,12 +6,22 @@
 /*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/14 12:14:34 by kibotrel          #+#    #+#             */
-/*   Updated: 2019/04/23 16:24:53 by kibotrel         ###   ########.fr       */
+/*   Updated: 2019/05/06 17:55:47 by kibotrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
+
+static void	free_split(char **tab)
+{
+	int	i;
+
+	i = -1;
+	while (tab[++i])
+		free(tab[i]);
+	free(tab);
+}
 
 char		**ft_strsplit(char const *s, char c)
 {
@@ -29,7 +39,11 @@ char		**ft_strsplit(char const *s, char c)
 			i++;
 		if (s[i] != '\0')
 		{
-			tab[j++] = ft_strsub(s, i, ft_wordlength(s, i, c));
+			if (!(tab[j++] = ft_strsub(s, i, ft_wordlength(s, i, c))))
+			{
+				free_split(tab);
+				return (NULL);
+			}
 			i += ft_wordlength(s, i, c);
 		}
 	}
