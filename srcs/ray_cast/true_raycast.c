b7e59775dec5579faeaeb86e69ace1d9a479a3c7
Remi DA CONCEICAO <reda-con@e2r12p4.42.fr>
2019-05-07 14:00:31 +0200

    raycasting is working

--- srcs/ray_cast/true_raycast.c
+++ srcs/ray_cast/true_raycast.c
@@ -6,7 +6,7 @@
 /*   By: nde-jesu <nde-jesu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/29 13:30:34 by nde-jesu          #+#    #+#             */
-/*   Updated: 2019/05/03 16:01:57 by reda-con         ###   ########.fr       */
+/*   Updated: 2019/05/07 13:52:42 by reda-con         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,13 +44,13 @@ static t_player	init_player(void)
 {
 	t_player	ret;
 
-	ret.play_pos.x = 1.5;
-	ret.play_pos.y = 3.5;
+	ret.play_pos.x = 8.5;
+	ret.play_pos.y = 8.5;
 	ret.play_coor.x = floor(ret.play_pos.x) * SQUARE_SIZE\
 					  + (ret.play_pos.x - floor(ret.play_pos.x)) * SQUARE_SIZE;
 	ret.play_coor.y = floor(ret.play_pos.y) * SQUARE_SIZE\
 					  + (ret.play_pos.y - floor(ret.play_pos.y)) * SQUARE_SIZE;
-	ret.play_angle = 90;
+	ret.play_angle = 135;
 	ret.play_angle = (ret.play_angle == 360) ? 0 : ret.play_angle;
 	return (ret);
 }
@@ -69,7 +69,6 @@ static t_ray_cast	y_collisions(t_ray_cast all, t_player play)
 	all.dist_col_y.x = SQUARE_SIZE / my_tan(rad_angle(all.act_angle));
 	all.dist_col_y.x = (all.act_angle > 180 && all.act_angle < 270)\
 					   ? -all.dist_col_y.x : all.dist_col_y.x;
-	printf("distY:%f; %f\n", all.dist_col_y.x, all.dist_col_y.y);
 	return (all);
 }
 
@@ -87,7 +86,6 @@ static t_ray_cast	x_collisions(t_ray_cast all, t_player play)
 	all.dist_col_x.y = SQUARE_SIZE * my_tan(rad_angle(all.act_angle));
 	all.dist_col_x.y = (all.act_angle > 0 && all.act_angle < 90)\
 					   ? -(all.dist_col_x.y) : all.dist_col_x.y;
-	printf("distX:%f; %f\n", all.dist_col_x.x, all.dist_col_x.y);
 	return (all);
 }
 
@@ -97,6 +95,14 @@ static t_ray_cast	check_neg(t_ray_cast all)
 	all.coll_y.y = (all.coll_y.y < 0) ? 0 : all.coll_y.y;
 	all.coll_x.x = (all.coll_x.x < 0) ? 0 : all.coll_x.x;
 	all.coll_x.y = (all.coll_x.y < 0) ? 0 : all.coll_x.y;
+	all.coll_y.x = (all.coll_y.x > 9 * SQUARE_SIZE)\
+				? 9 * SQUARE_SIZE: all.coll_y.x;
+	all.coll_y.y = (all.coll_y.y > 9 * SQUARE_SIZE)\
+				? 9 * SQUARE_SIZE: all.coll_y.y;
+	all.coll_x.x = (all.coll_x.x > 9 * SQUARE_SIZE)\
+				? 9 * SQUARE_SIZE: all.coll_x.x;
+	all.coll_x.y = (all.coll_x.y > 9 * SQUARE_SIZE)\
+				? 9 * SQUARE_SIZE: all.coll_x.y;
 	return (all);
 }
 
@@ -120,14 +126,15 @@ static t_ray_cast	check_coll(t_ray_cast all, int **map)
 		}
 		if (hit_y == 0 && map[(int)all.coll_y.y / SQUARE_SIZE]\
 				[(int)all.coll_y.x / SQUARE_SIZE] != 0)
+		{
 			hit_y = 1;
+		}
 		else if (hit_y == 0)
 		{
 			all.coll_y.x += all.dist_col_y.x;
 			all.coll_y.y += all.dist_col_y.y;
 		}
 	}
-	printf("X: %i; %i\nY: %i; %i\n", (int)all.coll_x.x/64, (int)all.coll_x.y/64, (int)all.coll_y.x/64, (int)all.coll_y.y/64);
 	return (all);
 }
 
@@ -140,7 +147,6 @@ void		true_raycast(int **map, SDL_Renderer *ren)
 	t_ptd		wall_start;
 	t_ptd		wall_end;
 
-	// (void)ren;
 	x = -1;
 	play = init_player();
 	all = init_rc(play);
@@ -149,12 +155,17 @@ void		true_raycast(int **map, SDL_Renderer *ren)
 		all = y_collisions(all, play);
 		all = x_collisions(all, play);
 		all = check_coll(all, map);
-		all.dist_col = (sqrt(pow(all.coll_x.x - play.play_coor.x, 2) + pow(all.coll_x.y - play.play_coor.y, 2)) >
-				sqrt(pow(all.coll_y.x - play.play_coor.x, 2) + pow(all.coll_y.y - play.play_coor.y, 2))) ?
-			sqrt(pow(all.coll_y.x - play.play_coor.x, 2) + pow(all.coll_y.y - play.play_coor.y, 2)) :
-			sqrt(pow(all.coll_x.x - play.play_coor.x, 2) + pow(all.coll_x.y - play.play_coor.y, 2));
+		all.dist_col = (sqrt(pow(all.coll_x.x - play.play_coor.x, 2)\
+					+ pow(all.coll_x.y - play.play_coor.y, 2))\
+				> sqrt(pow(all.coll_y.x - play.play_coor.x, 2)\
+					+ pow(all.coll_y.y - play.play_coor.y, 2)))\
+					   ? sqrt(pow(all.coll_y.x - play.play_coor.x, 2)\
+							   + pow(all.coll_y.y - play.play_coor.y, 2))\
+					   : sqrt(pow(all.coll_x.x - play.play_coor.x, 2)\
+							   + pow(all.coll_x.y - play.play_coor.y, 2));
 		all.dist_col *= cos(rad_angle(play.play_angle - all.act_angle));
 		wall_size = ceil((SQUARE_SIZE / all.dist_col) * all.dist_screen);
+		printf("%i\n", wall_size);
 		wall_start.x = WIN_WIDTH - x;
 		wall_start.y = (WIN_HEIGHT / 2) - (wall_size / 2);
 		wall_end.x = WIN_WIDTH - x;
@@ -179,21 +190,22 @@ int		main(void)
 	if (SDL_CreateWindowAndRenderer(WIN_WIDTH, WIN_HEIGHT, SDL_WINDOW_RESIZABLE,
 				&win, &ren) == -1)
 		return (1);
-	worldMap = malloc(sizeof(int*) * 4);
-	while (x < 4)
+	worldMap = malloc(sizeof(int*) * 10);
+	while (x < 10)
 		worldMap[x++] = malloc(sizeof(int) * 10);
 	x = -1;
-	while (++x < 4)
+	while (++x < 10)
 	{
 		y = -1;
 		while (++y < 10)
 		{
-			if (x == 0 || y == 0 || x == 3 || y == 9)
+			if (x == 0 || y == 0 || x == 9 || y == 9)
 				worldMap[x][y] = 1;
 			else
 				worldMap[x][y] = 0;
 		}
 	}
+	worldMap[3][3] = 1;
 	SDL_SetRenderDrawColor(ren, 0, 255, 0, 255);
 	true_raycast(worldMap, ren);
 	while (42)
