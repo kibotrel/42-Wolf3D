f8aff1acc920030cdf0809ab0013383732f05f18
Remi DA CONCEICAO <reda-con@e1r9p11.42.fr>
2019-05-14 11:06:34 +0200

    but we need this

--- srcs/raycasting/raycast_tools.c
+++ srcs/raycasting/raycast_tools.c
@@ -6,7 +6,7 @@
 /*   By: reda-con <reda-con@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/26 13:14:59 by reda-con          #+#    #+#             */
-/*   Updated: 2019/05/13 17:55:13 by kibotrel         ###   ########.fr       */
+/*   Updated: 2019/05/14 10:51:27 by reda-con         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include "env.h"
 #include <math.h>
 
-void	fun_exit(SDL_Renderer *ren, SDL_Window *win)
+void		fun_exit(SDL_Renderer *ren, SDL_Window *win)
 {
 	SDL_DestroyRenderer(ren);
 	SDL_DestroyWindow(win);
@@ -26,7 +26,7 @@ double	double_abs(double i)
 	return ((i > 0) ? i : -i);
 }
 
-void	draw_rc(t_pos start, t_pos end, SDL_Renderer *ren, int clr)
+void		draw_rc(t_pos start, t_pos end, SDL_Renderer *ren, int clr)
 {
 	t_pos	current;
 
@@ -43,4 +43,14 @@ void	draw_rc(t_pos start, t_pos end, SDL_Renderer *ren, int clr)
 		SDL_RenderDrawPoint(ren, (int)current.x, (int)current.y);
 		++current.y;
 	}
+}
+
+double		length(t_pos col_x, t_pos col_y, t_pos play_coor)
+{
+	double	is_y;
+	double	is_x;
+
+	is_x = sqrt(pow(col_x.x - play_coor.x, 2) + pow(col_x.y - play_coor.y, 2));
+	is_y = sqrt(pow(col_y.x - play_coor.x, 2) + pow(col_y.y - play_coor.y, 2));
+	return (is_x > is_y ? is_y : is_x);
 }
