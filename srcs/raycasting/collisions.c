e0b4ce9e90626e8eef5a9ecd34e926a194a9cd2d
Kilian BOTREL <kibotrel@e3r2p7.42.fr>
2019-05-27 18:11:39 +0200

    modified collision checker calls

--- srcs/raycasting/collisions.c
+++ srcs/raycasting/collisions.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   collisions.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nde-jesu <nde-jesu@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/10 18:16:08 by kibotrel          #+#    #+#             */
-/*   Updated: 2019/05/23 10:55:33 by reda-con         ###   ########.fr       */
+/*   Updated: 2019/05/27 17:17:18 by kibotrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,26 @@
 #include "env.h"
 #include "wolf3d.h"
 
-void		y_collisions(t_pos *hit_y, t_pos *gap_y, double angle, t_cam cam)
+void		y_collisions(t_ray *ray, t_cam cam, t_data data)
 {
-	if (angle > R_E && angle < R_W)
-		hit_y->y = floor(cam.coord.y / CELL) * CELL - 0.0000001;
+	if (ray->angle > data.east && ray->angle < data.west)
+		ray->hit_y.y = floor(cam.coord.y / CELL) * CELL - 0.0000001;
 	else
-		hit_y->y = floor(cam.coord.y / CELL) * CELL + CELL;
-	gap_y->y = (angle > R_E && angle < R_W) ? -CELL : CELL;
-	hit_y->x = cam.coord.x + (cam.coord.y - hit_y->y) / tan(angle);
-	gap_y->x = CELL / tan(angle);
-	gap_y->x = (angle > R_E && angle < R_W) ? gap_y->x : -gap_y->x;
+		ray->hit_y.y = floor(cam.coord.y / CELL) * CELL + CELL;
+	ray->gap_y.y = (ray->angle > data.east && ray->angle < data.west) ? -CELL : CELL;
+	ray->hit_y.x = cam.coord.x + (cam.coord.y - ray->hit_y.y) / tan(ray->angle);
+	ray->gap_y.x = CELL / tan(ray->angle);
+	ray->gap_y.x = (ray->angle > data.east && ray->angle < data.west ? ray->gap_y.x : -ray->gap_y.x);
 }
 
-void		x_collisions(t_pos *hit_x, t_pos *gap_x, double angle, t_cam cam)
+void		x_collisions(t_ray *ray, t_cam cam, t_data data)
 {
-	if (angle > R_N && angle < R_S)
-		hit_x->x = floor(cam.coord.x / CELL) * CELL - 0.0000001;
+	if (ray->angle > data.north && ray->angle < data.south)
+		ray->hit_x.x = floor(cam.coord.x / CELL) * CELL - 0.0000001;
 	else
-		hit_x->x = floor(cam.coord.x / CELL) * CELL + CELL;
-	gap_x->x = (angle > R_N && angle < R_S) ? -CELL : CELL;
-	hit_x->y = cam.coord.y + (cam.coord.x - hit_x->x) * tan(angle);
-	gap_x->y = CELL * tan(angle);
-	gap_x->y = (angle > R_N && angle < R_S) ? gap_x->y : -gap_x->y;
+		ray->hit_x.x = floor(cam.coord.x / CELL) * CELL + CELL;
+	ray->gap_x.x = (ray->angle > data.north && ray->angle < data.south) ? -CELL : CELL;
+	ray->hit_x.y = cam.coord.y + (cam.coord.x - ray->hit_x.x) * tan(ray->angle);
+	ray->gap_x.y = CELL * tan(ray->angle);
+	ray->gap_x.y = (ray->angle > data.north && ray->angle < data.south ? ray->gap_x.y : -ray->gap_x.y);
 }
