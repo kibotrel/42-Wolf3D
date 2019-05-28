b8623cbc79df565b9dd3ecf41c534e02e695a6a6
Remi DA CONCEICAO <reda-con@e2r5p12.42.fr>
2019-05-28 19:10:48 +0200

    surface start to work

--- srcs/raycasting/raycast_tools.c
+++ srcs/raycasting/raycast_tools.c
@@ -6,7 +6,7 @@
 /*   By: nde-jesu <nde-jesu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/26 13:14:59 by reda-con          #+#    #+#             */
-/*   Updated: 2019/05/28 15:58:36 by nde-jesu         ###   ########.fr       */
+/*   Updated: 2019/05/28 19:08:08 by reda-con         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,10 @@ void			fun_exit(SDL_Renderer *ren, SDL_Window *win)
 
 static int	get_pixel_surface(SDL_Surface *surf, int x, int y)
 {
-    int       *p;
+	uint8_t	*p;
 
-    p = surf->pixels + y * surf->pitch - x * surf->format->BytesPerPixel;
-	// printf("%0xi\n", *p);
+	p = surf->pixels + y * surf->pitch - x * surf->format->BytesPerPixel;
+//	printf("0x%0x\n", *p);
 	return (*p);
 }
 
@@ -35,7 +35,13 @@ void			draw_rc(t_pos start, t_pos end, t_env *env, t_ray *ray)
 	t_pos		current;
 	int			color;
 	int			unit_size;
+	uint8_t		r;
+	uint8_t		g;
+	uint8_t		b;
 
+	r = 0;
+	g = 0;
+	b = 0;
 	current.x = start.x;
 	current.y = 0;
 	unit_size = (int)ray->wall.size % CELL;
@@ -45,9 +51,10 @@ void			draw_rc(t_pos start, t_pos end, t_env *env, t_ray *ray)
 			color = 0xff00ffff;
 		else if (current.y >= start.y && current.y <= end.y)
 		{
-
-			color = get_pixel_surface(env->sdl.surf[ray->which_wall], ray->offset, (int)current.y % CELL);
-			
+			SDL_GetRGB(get_pixel_surface(env->sdl.surf[ray->which_wall], ray->offset, (int)current.y % CELL)
+				, env->sdl.surf[ray->which_wall]->format, &r, &g, &b);
+			//color = get_pixel_surface(env->sdl.surf[ray->which_wall], ray->offset, (int)current.y % CELL);
+			color = r << 16 | g << 8 | b;
 		}
 		else
 			color = 0xffc8c8c8;
