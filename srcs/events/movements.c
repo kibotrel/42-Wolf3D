98a7149b9efdf38f458666840b9c1e5aa1355d71
Remi DA CONCEICAO <reda-con@e3r4p6.42.fr>
2019-06-13 17:09:17 +0200

    slide in wall and hitbox

--- srcs/events/movements.c
+++ srcs/events/movements.c
@@ -6,38 +6,56 @@
 /*   By: nde-jesu <nde-jesu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/14 07:45:26 by nde-jesu          #+#    #+#             */
-/*   Updated: 2019/06/07 13:15:48 by reda-con         ###   ########.fr       */
+/*   Updated: 2019/06/13 17:06:34 by reda-con         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "env.h"
 #include "wolf3d.h"
 
-static void		coll_wall(t_env *env, t_pos coord, char *key)
+t_pos			init_pos(double x, double y)
 {
-	if (env->map[(int)coord.y / CELL][(int)coord.x / CELL] == 1)
-	{
-		if (key[SDL_SCANCODE_W])
-			move_backward(env, env->cam);
-		if (key[SDL_SCANCODE_S])
-			move_forward(env, env->cam);
-		if (key[SDL_SCANCODE_A])
-			move_right(env, env->cam);
-		if (key[SDL_SCANCODE_D])
-			move_left(env, env->cam);
-	}
+	t_pos ret;
+
+	ret.x = x;
+	ret.y = y;
+	return (ret);
+}
+
+static void		movements(t_cam *cam, t_pos old, t_env *env, t_pos fl)
+{
+	t_pos	new;
+	double	hit;
+
+	new.x = cam->coord.x + (cos(radians(cam->angle + fl.x)) * (8 * cam->sprint)) * fl.y;
+	new.y = cam->coord.y - (sin(radians(cam->angle + fl.x)) * (8 * cam->sprint)) * fl.y;
+	if (new.y < old.y)
+		hit = -HITBOX;
+	else
+		hit = HITBOX;
+	if (env->map[(int)(new.y / CELL + hit)][(int)old.x / CELL] != 1)
+		cam->coord.y = new.y;
+	if (new.x < old.x)
+		hit = -HITBOX;
+	else
+		hit = HITBOX;
+	if (env->map[(int)old.y / CELL][(int)(new.x / CELL + hit)] != 1)
+		cam->coord.x = new.x;
 }
 
 void			move(t_env *env, char *key, t_pos *flags)
 {
+	t_pos	old;
+
+	old.x = env->cam.coord.x;
+	old.y = env->cam.coord.y;
 	if (key[SDL_SCANCODE_W])
-		move_forward(env, env->cam);
+		movements(&env->cam, old, env, init_pos(0, 1));
 	if (key[SDL_SCANCODE_S])
-		move_backward(env, env->cam);
+		movements(&env->cam, old, env, init_pos(0, -1));
 	if (key[SDL_SCANCODE_A])
-		move_left(env, env->cam);
+		movements(&env->cam, old, env, init_pos(90, 1));
 	if (key[SDL_SCANCODE_D])
-		move_right(env, env->cam);
-	coll_wall(env, env->cam.coord, key);
+		movements(&env->cam, old, env, init_pos(90, -1));
 	flags->y = 1;
 }
